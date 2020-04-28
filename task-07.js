b220e31542a0afa0f721aebd578a84c19543c826
IgorVerchyk <sabyr.ivua@gmail.com>
2020-04-28 17:35:21 +0300

    28/04/20

--- task-07.js
+++ task-07.js
@@ -3,12 +3,12 @@ const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 const isLoginValid = (login) => login.length >= 4 && login.length <= 16;
 
 const isLoginUnique = function (logins, login) {
-  return logins.includes(login) ? true : false;
+  return logins.includes(login) ? false : true;
 };
 const addLogin = function (logins, login) {
   if (isLoginValid(login) === false) {
     return "Ошибка! Логин должен быть от 4 до 16 символов";
-  } else if (isLoginUnique(logins, login) === true) {
+  } else if (isLoginUnique(logins, login) === false) {
     return "Такой логин уже используется!";
   } else {
     logins.push(login);
